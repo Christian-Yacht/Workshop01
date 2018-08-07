afa6dfd38f6671d4d7bf98248434fef9692d6feb
Christian-Yacht <37537687+Christian-Yacht@users.noreply.github.com>
2018-08-07 17:53:48 +0200

    dismissbutton + id/title# new client update

--- workshop01/src/App.js
+++ workshop01/src/App.js
@@ -1,60 +1,99 @@
 import React, { Component } from 'react';
 
-const Title=() => (
+const Title = () => (
   <h2>List of News Clients</h2>
 )
 
 class App extends Component {
   state = {
-    data: [    {
-       title: 'React',
-       url: 'https://facebook.github.io/react/',
-       author: 'Jordan Walke',
-       num_comments: 3,
-       points: 4,
-       objectID: 0,
-   }, {
-       title: 'Redux',
-       url: 'https://github.com/reactjs/redux',
-       author: 'Dan Abramov, Andrew Clark',
-       num_comments: 2,
-       points: 5,
-       objectID: 1,
-   }, ]
+    data: [{
+      id: 0,
+      title: 'React',
+      url: 'https://facebook.github.io/react/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    }, {
+      id: 1,
+      title: 'Redux',
+      url: 'https://github.com/reactjs/redux',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },]
   }
 
-  addNewClient=() => {
-    this.setState({data: [
-      //To get the entire list when updating the list (?)
-      ...this.state.data,
+  addNewClient = () => {
+    //following line added to give new client new ID. 
+    //it looks at ID of last item in list and adds 1.
+    const clientId = this.state.data[this.state.data.length-1].id + 1;
+    this.setState({
+      
+      data: [
+        //To get the entire list when updating the list (?)
+        ...this.state.data,
 
-      //the (standard) new client
-       {title: 'Chris',
-       url: 'https://www.linkedin.com/in/christian-van-den-broeck-920077150/.',
-       author: 'Christian van den Broeck',
-       num_comments: 2,
-       points: 5,
-       objectID: 1,}
-    ]})
+        //the (standard) new client
+        {
+          id: clientId,
+          title: 'Chris',
+          url: 'https://www.linkedin.com/in/christian-van-den-broeck-920077150/.',
+          author: 'Christian van den Broeck',
+          num_comments: 2,
+          points: 5,
+          objectID: 1,
+        }
+      ]
+    })
   }
+
+  removeClient = (clientId) => {
+    const clients = this.state.data.filter(function(kees) { return kees.id !== clientId});
+    this.setState({
+      data: clients
+    })
+  }
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // removeClient = (clientId) => {
+  //   const clients = this.state.data.filter(function(client) { return client.id != clientId});
+  //   this.setState({
+  //     data: clients
+  //   })
+  // }
 
   render() {
     return (
       <div className="App">
-             <Title />
-
-             {/*creating a list with elements from Data.
+        <Title />
+        {/*creating a list with elements from Data.
               .map creates a 'map' so that every list elements has more than one data item */}
-      {this.state.data
-      .map((i) => <div key={i.title}>
-      <li>Title:<a href={i.url}>{i.title}</a></li>
-      <li>Author: {i.author}</li>
-      <li>Number of comments: {i.num_comments}</li>
-      <li>Number of points: {i.points}</li>
-      <hr/></div>)}
+        {this.state.data
+          .map((i) => 
+            <div key={i.id} className="client">
+              <ul>
+                <li>Title: {i.id} -- <a href={i.url}>{i.title}</a></li>
+                <li>Author: {i.author}</li>
+                <li>Number of comments: {i.num_comments}</li>
+                <li>Number of points: {i.points}</li>
+              </ul>  
+              <button onClick={() => this.removeClient(i.id)}>Dismiss</button>
+              <hr />
+            </div>)
+        }
 
-      {/*to put a button on bottom of the list. arrow statement used*/}
-      <button onClick={() => this.addNewClient()}>Add Standard Client</button>
+        {/*to put a button on bottom of the list. arrow statement used*/}
+        <button onClick={() => this.addNewClient()}>Add Standard Client</button>
       </div>
     );
   }
