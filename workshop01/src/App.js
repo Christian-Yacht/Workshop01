816b4438240f00d651a8cd216604a91fa10a0f4d
Christian-Yacht <37537687+Christian-Yacht@users.noreply.github.com>
2018-08-07 14:42:49 +0200

    foute changes

--- workshop01/src/App.js
+++ workshop01/src/App.js
@@ -1,67 +1,13 @@
 import React, { Component } from 'react';
+import sampleData from './Data/SampleData';
 
 const Title=() => (
   <h2>List of News Clients</h2>
 )
 
 class App extends Component {
-  state = {
-    data: [    {
-       title: 'React',
-       url: 'https://facebook.github.io/react/',
-       author: 'Jordan Walke',
-       num_comments: 3,
-       points: 4,
-       objectID: 0,
-   }, {
-       title: 'Redux',
-       url: 'https://github.com/reactjs/redux',
-       author: 'Dan Abramov, Andrew Clark',
-       num_comments: 2,
-       points: 5,
-       objectID: 1,
-   }, ]
-  }
-
-  //dismiss button function. Doesn't work like this :')
-  /*dismissClient=() => {
-    this.setState(
-      this.data.remove()
-    )
-  }*/
-
-  //doesn't work either... 'TypeError: Cannot read property 'parentNode' of null'
-  //this came (partially) from stackoverflow
-  /*dismissClient=() => {
-    var element = document.getElementById('data');
-    element.parentNode.removeChild(element);
-    return false;
-  }*/
-
-  //almost works :'). only dismisses 'React', even if you click dismiss on 'Redux'.
-  //after clicking again, react appears again (basicly only 'hides' and 'shows' the first item)
-  dismissClient() {
-    var element = document.getElementById("data");
-    if (element.style.display === "none") {
-        element.style.display = "block";
-        } else {
-           element.style.display = "none";
-               }
-              }
-  
-    //almost works :'). only deletes data items from the list item (and after first list item is deleted, an error occurs)
-    removeClient()
-    {
-      var list = document.getElementById("data");
-        list.removeChild(list.childNodes[0]);
-
-        this.setState();
-      
-    }
-  
-
   addNewClient=() => {
-    this.setState({data: [
+    /*this.setState({data: [
       //To get the entire list when updating the list (?)
       ...this.state.data,
 
@@ -71,9 +17,27 @@ class App extends Component {
        author: 'Christian van den Broeck',
        num_comments: 2,
        points: 5,
-       objectID: 1,}
-    ]})
+       objectID: 2,}
+    ]})*/
+    console.log('jumbo');
   }
+  
+  render(){
+    const {list} = this.state;
+    return(
+        <table>
+            {list.map(article => {
+            return (
+                <sampleData
+                key = {article.objectID}
+                article = {article}
+                onClick={this.removeItem(article.objectID)}
+                />
+            );
+            })}
+        </table>
+    )
+}
 
   render() {
     //hiermee kan je alle info zien waarmee je kan werken(?)
@@ -89,10 +53,9 @@ class App extends Component {
       .map((i) => <div key={i.title} id="data">
       <li>Title:<a href={i.url}>{i.title}</a></li>
       <li>Author: {i.author}</li>
-      <li>Number of comments: {i.num_comments}</li>
-      <li>Number of points: {i.points}</li>
+      <li>Number of comments: {i.num_comments}</li>      <li>Number of points: {i.points}</li>
       {/*dismiss button (doesn't function yet)*/}
-      <button onClick={()=> this.removeClient()}>Dismiss</button>
+      <button onClick={()=> this.dismissClient()}>Dismiss</button>
       <hr/></div>)}
 
       {/*to put a button on bottom of the list. arrow statement used*/}
@@ -100,26 +63,6 @@ class App extends Component {
       </div>
     );
   }
-
-  //filter function found on the internet. Don't know how to alter this to make it work
-  filterFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
 }
 
 export default App;
